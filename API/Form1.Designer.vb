2a76085f7671b20cd640f9e53c8156d8b5a4c396
adistiradyiputra <adistiraxx77@gmail.com>
2022-03-31 20:01:52 +0700

    Tugas IPAT AdistiraDyiputra-TI(A)

---
+++ API/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.npm = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.jurusan = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.prodi = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.kelas = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 404)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Mahasiswa"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.npm, Me.nama, Me.jurusan, Me.prodi, Me.kelas})
+        Me.DataGridView1.Location = New System.Drawing.Point(117, 31)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(540, 345)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'npm
+        '
+        Me.npm.HeaderText = "Npm"
+        Me.npm.Name = "npm"
+        '
+        'nama
+        '
+        Me.nama.HeaderText = "Nama"
+        Me.nama.Name = "nama"
+        '
+        'jurusan
+        '
+        Me.jurusan.HeaderText = "Jurusan"
+        Me.jurusan.Name = "jurusan"
+        '
+        'prodi
+        '
+        Me.prodi.HeaderText = "Prodi"
+        Me.prodi.Name = "prodi"
+        '
+        'kelas
+        '
+        Me.kelas.HeaderText = "Kelas"
+        Me.kelas.Name = "kelas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "VB.Net With Rest API CI3"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents npm As DataGridViewTextBoxColumn
+    Friend WithEvents nama As DataGridViewTextBoxColumn
+    Friend WithEvents jurusan As DataGridViewTextBoxColumn
+    Friend WithEvents prodi As DataGridViewTextBoxColumn
+    Friend WithEvents kelas As DataGridViewTextBoxColumn
+End Class
---
+++ API/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Nancy.Json
+
+Public Class Form1
+
+    Private Sub getdata()
+        Try
+
+            Dim uriString As String = "http://localhost/CodeIgniter_API/index.php/api/GetData"
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "GET"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("npm").ToString, item("nama").ToString, item("jurusan").ToString, item("prodi").ToString, item("kelas").ToString)
+
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        getdata()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
